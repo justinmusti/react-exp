2e1aa4038f13f64cfdb993c0341963822332c539
justinmusti <mustiatwork@gmail.com>
2019-02-08 16:34:26 -0800

    Test UserView Logout.
    Added a new test to verify user logout process.

--- rexp/user/tests.py
+++ rexp/user/tests.py
@@ -53,5 +53,27 @@ class UserViewTest(unittest.TestCase):
         response = self.client.post(login_url, {'username': self.username, 'password': self.password})
         self.assertEqual(response.status_code, 200)
 
+    def test_logout_process(self):
+        """
+        Note: this is NOT an IDEAL APPROACH | DONT do this
+        --------------------------------------------------
+        Step 1: Log user in with built-in login method of Client
+        Step 2: Verify user is_authenticated property evaluates to True
+        Step 3: Log user out with logout end-point
+        Step 4: Verify user is_authenticated property now evaluates to False
+        PS: ****> Acquire user object from self.Client to ensure changes to is properties are reflected <****
 
+        :return:
+        """
+        # login user
+        self.client.login(username=self.username, password=self.password)
+        # Test user is authenticated
+        logged_in_user = self.client.request().context['user']
+        self.assertEqual(logged_in_user.is_authenticated, True)
 
+        # Logout user
+        logout_url = reverse('user:logout')
+        response = self.client.get(logout_url)
+        self.assertEqual(response.status_code, 302)
+        # test user is Anonymous after logout
+        self.assertEqual(self.client.request().context['user'].is_authenticated, False)
